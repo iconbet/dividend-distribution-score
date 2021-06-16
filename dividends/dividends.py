f9cfd30a1ee070ee933fdab15330c00cf1b947f9
cupiddev <cupiddev@protonmail.com>
2021-06-16 11:21:51 +0545

    add name() for the contract

--- dividends/dividends.py
+++ dividends/dividends.py
@@ -233,6 +233,10 @@ class Dividends(IconScoreBase):
             response[category] = self._dividend_percentage.get(index)
         return response
 
+    @external(readonly=True)
+    def name(self) -> str:
+        return TAG
+
     @external
     def set_token_score(self, _score: Address) -> None:
         """
